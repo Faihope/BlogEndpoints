c0a0b95e8ae5980f44aaada0eb3c526c3c1ea4a2
Faihope <Faihope>
2022-06-22 01:47:04 +0300

    add login and register fields and data

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1po55!c&lm2^o=9_qyb4b$k81rd^@&y6ywycy#_cc(%^d2dd7@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
